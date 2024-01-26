4330bcea741583f42d96ddb76782723eaf61259f
RitaMulato <ritaamandio96@gmail.com>
2024-01-26 20:51:11 +0100

    "feat: spring security"

--- src/main/java/com/bluehouse/bluehouse/config/SecurityConfig.java
+++ src/main/java/com/bluehouse/bluehouse/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth->auth
                                 .requestMatchers("/assets/**").permitAll()
-                                .requestMatchers("/").permitAll()
+                                .requestMatchers("/").hasAnyAuthority("admin", "user")
                                 .requestMatchers("/funcionarios/**").hasAnyAuthority("admin")
                                 .requestMatchers("/medidasDisciplinares/**").hasAnyAuthority("admin")
                                 .anyRequest().authenticated())
